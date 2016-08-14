b132800c3edb95b675691be155bbcaf0e1b55c44
Mateusz Misztoft <mateusz.misztoft@gmail.com>
2016-08-14 23:44:48 +0200

    add main loop

--- src/js/engine.js
+++ src/js/engine.js
@@ -1,7 +1,37 @@
-var g = function () { this.init() }
+var E = function () {
+    this.init()
+}
 
-g.prototype = {
+E.prototype = {
     init: function () {
-        console.log('init')
+        this.time = Date.now()
+        requestAnimFrame(this.loop.bind(this))
+    },
+    step: function (dt) {
+    },
+    render: function (dt) {
+    },
+    loop: function () {
+        var timeNow = Date.now()
+        this.dt = (timeNow - this.time) / 1000
+        this.time = timeNow
+        this.step(this.dt)
+        this.render(this.dt)
+        requestAnimFrame(this.loop.bind(this))
     }
 }
+
+var r = function (a, b, c) {
+    b = document, c = 'addEventListener'
+    b[c] ? b[c]('DOMContentLoaded', a) : window.attachEvent('onload', a)
+}
+
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+        window.setTimeout(callback, 1000 / 60)
+    }
+})()
+
+r (function () {
+    new E()
+})
